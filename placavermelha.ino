3bab7d0fa219bcf6c6c375c9a3e5d727bf5afa1f
lksmaster <47121158+lksmaster@users.noreply.github.com>
2019-02-08 18:03:24 -0200

    Add files via upload

---
+++ placavermelha.ino
@@ -0,0 +1,174 @@
+#include <QTRSensors.h>
+
+// This example is designed for use with six QTR-1A sensors or the first six sensors of a
+// QTR-8A module.  These reflectance sensors should be connected to analog inputs 0 to 5.
+// The QTR-8A's emitter control pin (LEDON) can optionally be connected to digital pin 2,
+// or you can leave it disconnected and change the EMITTER_PIN #define below from 2 to
+// QTR_NO_EMITTER_PIN.
+
+// The setup phase of this example calibrates the sensor for ten seconds and turns on
+// the LED built in to the Arduino on pin 13 while calibration is going on.
+// During this phase, you should expose each reflectance sensor to the lightest and
+// darkest readings they will encounter.
+// For example, if you are making a line follower, you should slide the sensors across the
+// line during the calibration phase so that each sensor can get a reading of how dark the
+// line is and how light the ground is.  Improper calibration will result in poor readings.
+// If you want to skip the calibration phase, you can get the raw sensor readings
+// (analog voltage readings from 0 to 1023) by calling qtra.read(sensorValues) instead of
+// qtra.readLine(sensorValues).
+
+// The main loop of the example reads the calibrated sensor values and uses them to
+// estimate the position of a line.  You can test this by taping a piece of 3/4" black
+// electrical tape to a piece of white paper and sliding the sensor across it.  It
+// prints the sensor values to the serial monitor as numbers from 0 (maximum reflectance)
+// to 1000 (minimum reflectance) followed by the estimated location of the line as a number
+// from 0 to 5000.  1000 means the line is directly under sensor 1, 2000 means directly
+// under sensor 2, etc.  0 means the line is directly under sensor 0 or was last seen by
+// sensor 0 before being lost.  5000 means the line is directly under sensor 5 or was
+// last seen by sensor 5 before being lost.
+
+#define NUM_SENSORS             8  // number of sensors used
+#define NUM_SAMPLES_PER_SENSOR  4  // average 4 analog samples per sensor reading
+#define EMITTER_PIN             2  // emitter is controlled by digital pin 2
+
+// sensors 0 through 5 are connected to analog inputs 0 through 5, respectively
+QTRSensorsAnalog qtra((unsigned char[]) {A0, A1, A2, A3, A4, A5, A7, A6},
+  NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
+unsigned int sensorValues[NUM_SENSORS];
+unsigned int last_proportional = 0;
+long integral = 0;
+const int motorEsq[3] = {7, 6, 5};
+const int motorDir[3] = {9, 10, 11};
+
+void setup()
+{
+  delay(500);
+  
+ for(int i = 0; i<3; i++){
+  pinMode(motorEsq[i], OUTPUT);
+  pinMode(motorDir[i], OUTPUT);
+ }
+  
+  //Serial.begin(9600);
+  //Serial.println("comecou calibracao!");
+  para();
+  for (int i = 0; i < 10; i++)  // make the calibration take about 10 seconds
+  {
+    qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
+  }
+  for (int i = 0; i < 80; i++)  // make the calibration take about 10 seconds
+  {
+    qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
+  }
+  for (int i = 0; i < 10; i++)  // make the calibration take about 10 seconds
+  {
+    qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
+  }
+  para();
+  // print the calibration minimum values measured when emitters were on
+  
+//  for (int i = 0; i < NUM_SENSORS; i++)
+//  {
+//    Serial.print(qtra.calibratedMinimumOn[i]);
+//    Serial.print(' ');
+//  }
+//  Serial.println();
+//
+//  // print the calibration maximum values measured when emitters were on
+//  for (int i = 0; i < NUM_SENSORS; i++)
+//  {
+//    Serial.print(qtra.calibratedMaximumOn[i]);
+//    Serial.print(' ');
+//  }
+//  Serial.println();
+//  Serial.println();
+  delay(2000);
+}
+
+
+void loop()
+{
+  // read calibrated sensor values and obtain a measure of the line position from 0 to 5000
+  // To get raw sensor values, call:
+  //  qtra.read(sensorValues); instead of unsigned int position = qtra.readLine(sensorValues);
+  unsigned int linePosition = qtra.readLine(sensorValues);
+
+  // print the sensor values as numbers from 0 to 1000, where 0 means maximum reflectance and
+  // 1000 means minimum reflectance, followed by the line position
+//  for (unsigned char i = 0; i < NUM_SENSORS; i++)
+//  {
+//    Serial.print(sensorValues[i]);
+//    Serial.print('\t');
+//  }
+//  //Serial.println(); // uncomment this line if you are using raw values
+//  Serial.println(linePosition); // comment this line out if you are using raw values
+
+  // The "proportional" term should be 0 when we are on the line.
+  int proportional = (int)linePosition - 3500;
+
+  // Compute the derivative (change) and integral (sum) of the
+  // position.
+  int derivative = proportional - last_proportional;
+  integral += proportional;
+
+  // Remember the last position.
+  last_proportional = proportional;
+
+  // Compute the difference between the two motor power settings,
+  // m1 - m2.  If this is a positive number the robot will turn
+  // to the right.  If it is a negative number, the robot will
+  // turn to the left, and the magnitude of the number determines
+  // the sharpness of the turn.  You can adjust the constants by which
+  // the proportional, integral, and derivative terms are multiplied to
+  // improve performance.
+  int power_difference = proportional*1/5 + integral/5000 + derivative*17/15;
+
+  // Compute the actual motor settings.  We never set either motor
+  // to a negative value.
+  const int maximum = 180;
+  if (power_difference > maximum)
+    power_difference = maximum;
+  if (power_difference < -maximum)
+    power_difference = -maximum;
+
+  if (power_difference < 0)
+    anda(maximum + power_difference, maximum);
+  else
+    anda(maximum, maximum - power_difference);
+
+  
+}
+
+void anda(int velE, int velD) 
+{
+  if (velE >= 0){
+    digitalWrite(motorEsq[0], HIGH);
+    digitalWrite(motorEsq[1], LOW);
+    analogWrite(motorEsq[2], velE);
+  }else{
+    digitalWrite(motorEsq[0], LOW);
+    digitalWrite(motorEsq[1], HIGH);
+    analogWrite(motorEsq[2], -velE);
+  }
+  if (velD >= 0){
+    digitalWrite(motorDir[0], HIGH);
+    digitalWrite(motorDir[1], LOW);
+    analogWrite(motorDir[2], velD);
+  }else{
+    digitalWrite(motorDir[0], LOW);
+    digitalWrite(motorDir[1], HIGH);
+    analogWrite(motorDir[2], -velD);
+  }   
+}
+
+void para() 
+{
+  digitalWrite(motorEsq[0], LOW);
+  digitalWrite(motorEsq[1], LOW);
+  analogWrite(motorEsq[2], 0);
+
+    
+  digitalWrite(motorDir[0], LOW);
+  digitalWrite(motorDir[1], LOW);
+  analogWrite(motorDir[2], 0);
+}
