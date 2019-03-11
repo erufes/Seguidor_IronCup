26ef22f983828bd4b25ba5ba70b68aa44cdf4ab8
harmonia2side <breno_odnan@hotmail.com>
2019-03-11 18:58:45 -0300

    Sensor_chegada.cpp foi uma tentativa falha e não está sendo utilizada.
    Corrida_inicio-fim_2motores.ino é o novo código principal para a lógica 
    de corrida, mas ainda não está operacional.
    
    Usem outros códigos para testar o controle
    
    O controle mais recente será passado para o código principal em breve.

--- Corrida_inicio-fim_2motores.ino
+++ Corrida_inicio-fim_2motores.ino
@@ -1,5 +1,5 @@
 #include <QTRSensors.h>
-
+//------CONSTANTES------
 #define NUM_SENSORS             8  // number of sensors used
 #define NUM_SAMPLES_PER_SENSOR  4  // average 4 analog samples per sensor reading
 #define EMITTER_PIN             2  // emitter is controlled by digital pin 2
@@ -14,8 +14,14 @@ long integral = 0;
 const int motorEsq[3] = {6, 7, 5};
 const int motorDir[3] = {10, 9, 11};
 
-int pin_linha_chegada = 8;
+const int pin_linha_chegada = 8;
+
+//------VARIÁVEIS------
 int linha_chegada = 0;
+
+int read = 0, lastread = 0;//leituras de linha de chegada
+
+int t0 = 0;//variável de temporizador
 
 void setup() {
   Serial.begin(9600);
@@ -34,7 +40,6 @@ void setup() {
     qtra.calibrate();
   }
   delay(300);
-
 }
 
 
@@ -43,8 +48,26 @@ void loop() {
   //Aguardar botão
 
   //Começar corrida
-  while(!linha_chegada){
-  // ------CÓDIGO DE CONTROLE AQUI------
+  last_read = digitalRead(pin_linha_chegada);
 
-}//in progress
+  while(){//loop de corrida
+    // ------CÓDIGO DE CONTROLE AQUI------
+    //------CÓDIGO DE CONTROLE AQUI------ FIM
+
+    //colocar filtro de curva aqui
+    read = digitalRead(pin_linha_chegada);
+    if(linePosition >= 0 && linePosition <= 7000){//verifica se estamos no caminho certo
+      if(millis() - t0 > 2000){//verifica estouro de timer
+        if(read != lastread){
+          if(!last_read && read){
+            //detecta borda de subida com sucesso
+            linha_chegada = 1;
+            t0 = millis();//inicia timer
+          }
+          last_read = read;
+        }
+      }
+    }
+  }
+  //protocolo de parada
 }
---
+++ classes/Sensor_chegada.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class Sensor_chegada{
+public:
+  int pin;
+  int last_read;
+  bool key[3];
+
+  
+}
