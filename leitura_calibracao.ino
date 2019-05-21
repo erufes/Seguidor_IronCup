89f8cdb786f863e91a26bee0bb90803c7a3c9af2
lksmaster <47121158+lksmaster@users.noreply.github.com>
2019-05-21 17:50:56 -0300

    testado com a placa vermelha v1

--- leitura_calibracao.ino
+++ leitura_calibracao.ino
@@ -1,14 +1,15 @@
-#define NUM_SENSORS             7  // number of sensors used
+#define NUM_SENSORS             8  // number of sensors used
 #define NUM_SAMPLES_PER_SENSOR  4  // average 4 analog samples per sensor reading
+#define VELMIN 75
 
-int sensor[NUM_SENSORS] = {A0, A1, A2, A3, A4, A5, A6};   //conferir pinagem!
-const int motorEsq[3] = {7, 6, 5};                        //
-const int motorDir[3] = {9, 10, 11};                      //
+int sensor[NUM_SENSORS] = {A7, A6, A5, A4, A3, A2, A1, A0};   //conferir pinagem!
+const int motorEsq[3] = {11, 10, 9};                        //
+const int motorDir[3] = {5, 4, 3};                      //
 
-unsigned int values[NUM_SENSORS];
+int values[NUM_SENSORS];
 int calibratedMinimum[NUM_SENSORS];
 int calibratedMaximum[NUM_SENSORS];
-
+int lastValue;
 
 
 //***************************************************************
@@ -22,22 +23,22 @@ void setup()
     pinMode(motorDir[i], OUTPUT);
   }
   para();
-  
-  for(int i = 0; i < NUM_SENSORS; i++){
+
+  for (int i = 0; i < NUM_SENSORS; i++) {
     calibratedMinimum[i] = 999;
     calibratedMaximum[i] = 0;
   }
-  
+
   Serial.begin(9600);
   Serial.println("comecou calibracao!");
-  
-  for (int i = 0; i < 5; i++)
+
+  for (int i = 0; i < 6; i++)
   {
-    if (i % 2 == 0) {
-      anda(33, -33);
-    } else {
-      anda(-33, 33);
-    }
+    //    if (i % 2 == 0) {
+    //      anda(33, -33);
+    //    } else {
+    //      anda(-33, 33);
+    //    }
     for (int j = 0; j < 15 ; j++) {
       calibrate();
     }
@@ -45,7 +46,7 @@ void setup()
 
   printaCalibracao();
 
-  posCalibracao();
+  //posCalibracao();
 
   delay(800);
 }
@@ -53,12 +54,13 @@ void setup()
 
 void loop()
 {
-  readLine();
+  int lineposition = readLine();
 
-  for(int i = 0; i < NUM_SENSORS; i++){
+  for (int i = 0; i < NUM_SENSORS; i++) {
     Serial.print(values[i]);
     Serial.print(" ");
   }
+  Serial.print(lineposition);
   Serial.print("\n");
 }
 
@@ -68,20 +70,19 @@ void loop()
 
 //***************************************************************
 
-void calibrate(){
-  int value;
-  int soma;
-  for(int j = 0; j < NUM_SENSORS; j++){
-    soma = 0;
-    for(int i = 0; i < NUM_SAMPLES_PER_SENSOR; i++){
+void calibrate() {
+  for (int k = 0; k < 10; k++) {
+    int value;
+    int soma;
+    for (int j = 0; j < NUM_SENSORS; j++) {
       value = analogRead(sensor[j]);
-      soma += value;
+
+      if (value > calibratedMaximum[j])
+        calibratedMaximum[j] = value;
+      if (value < calibratedMinimum[j])
+        calibratedMinimum[j] = value;
     }
-    value = soma / NUM_SENSORS;
-    if(value > calibratedMaximum[j])
-      calibratedMaximum[j] = value;
-    if(value < calibratedMinimum[j])
-      calibratedMinimum[j] = value;
+    delay(8);
   }
 }
 
@@ -89,7 +90,7 @@ void calibrate(){
 void posCalibracao() {
 
   unsigned int linePosition;
-  
+
   while (linePosition < 3450 || linePosition > 3550) {
     linePosition = readLine();
     int erro = (int)(linePosition - 3500) / 14;
@@ -102,7 +103,7 @@ void posCalibracao() {
 }
 
 //printa os valores maximos e mininos da calibracao
-void printaCalibracao(){
+void printaCalibracao() {
   for (int i = 0; i < NUM_SENSORS; i++)
   {
     Serial.print(calibratedMinimum[i]);
@@ -119,22 +120,53 @@ void printaCalibracao(){
   Serial.println();
 }
 
-//le os sensores e calcula a posicao da linha 
-int readLine(){
-  int linePosition = 0;
-  int value;
-  int soma;
-  for(int j = 0; j < NUM_SENSORS; j++){
-    soma = 0;
-    for(int i = 0; i < NUM_SAMPLES_PER_SENSOR; i++){
+
+//le os sensores e calcula a posicao da linha
+int readLine()
+{
+  int linePosition = 0, on_line = 0;
+  long int value;
+  unsigned long avg = 0; // this is for the weighted total, which is long before division
+  unsigned long sum = 0; // this is for the denominator which is <= 64000
+
+  for (int j = 0; j < NUM_SENSORS; j++) {
+    int samples = 0;
+    for (int i = 0; i < NUM_SAMPLES_PER_SENSOR; i++) {
       value = analogRead(sensor[j]);
-      soma += value;
+      samples += value;
     }
-    value = soma / NUM_SAMPLES_PER_SENSOR;
-    values[j] = (value - calibratedMinimum[j])*1000/(calibratedMaximum[j] - calibratedMaximum[j]);   //calcula o valor de acordo com a calibracao
-    linePosition += values[j] * (j*1000);  
+    value = samples / NUM_SAMPLES_PER_SENSOR;
+
+    //calcula o valor de acordo com a calibracao
+    value = ((value - calibratedMinimum[j]) * 1000) / (calibratedMaximum[j] - calibratedMinimum[j]);
+    values[j] = value;
+    if (values[j] < 5) values[j] = 0;
+
+    // keep track of whether we see the line at all
+    if (value > 200) {
+      on_line = 1;
     }
-    return linePosition;
+    // only average in values that are above a noise threshold
+    if (value > 50) {
+      avg += (long)(value) * 1000 * j;
+      sum += value;
+    }
+  }
+
+  if (!on_line)
+  {
+    // If it last read to the left of center, return 0.
+    if (lastValue < (NUM_SENSORS - 1) * 1000 / 2)
+      return 0;
+
+    // If it last read to the right of center, return the max.
+    else
+      return (NUM_SENSORS - 1) * 1000;
+  }
+
+  lastValue = avg / sum;
+
+  return lastValue;
 }
 
 void anda(int velE, int velD)
@@ -159,13 +191,13 @@ void anda(int velE, int velD)
   }
 }
 
-void para() 
+void para()
 {
   digitalWrite(motorEsq[0], LOW);
   digitalWrite(motorEsq[1], LOW);
   analogWrite(motorEsq[2], 0);
 
-    
+
   digitalWrite(motorDir[0], LOW);
   digitalWrite(motorDir[1], LOW);
   analogWrite(motorDir[2], 0);
