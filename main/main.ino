8347e7b644e4a35ea9e1c0436f14cf00907ddc75
lksmaster <47121158+lksmaster@users.noreply.github.com>
2019-09-03 18:27:52 -0300

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,6 @@
 #define NUM_SAMPLES_PER_SENSOR  4    // samples per sensor reading
-#define VELMIN                  100   // velocidade minima
-#define VELMAX                  170  // velocidade maxima
+#define VELMIN                  95   // velocidade minima
+#define VELMAX                  150  // velocidade maxima
 #define USANDO_PLACA_VERMELHA        //Se usando placa vermelha, deixe essa linha aqui. Se não, comente ela
 
 #ifdef USANDO_PLACA_VERMELHA
@@ -11,15 +11,16 @@ const int sensor[NUM_SENSORS] = {A7, A6, A5, A4, A3, A2, A1, A0};   //sensores d
 const int sensor[NUM_SENSORS] = {A5, A4, A3, A2, A1, A0};   //sensores de linha
 #endif 
 
-const int motorEsq[3] = {11, 10, 9};  // {dig, dig, pwm}
-const int motorDir[3] = {5, 4, 3};    // {dig, dig, pwm}
-const int pin_chegada = 12;            // sensor de linha de chegada
-const int pin_curva = 2;//COLOCAR PINO              // sensor de curva
-const int pin_led = 0;//COLOCAR PINO
+const int motorEsq[3] = {7, 6, 10};  // {dig, dig, pwm} //CONFERIR SE ESTA CERTO!
+const int motorDir[3] = {8, 9, 5};    // {dig, dig, pwm}
+const int pin_chegada = 12;           // sensor de linha de chegada
+const int pin_curva = 11;              // sensor de curva
+const int pin_led = 13;               // led para indicar estados/erros
+const int pin_botao = 4;              // botao para acionar o robo
 
 int values[NUM_SENSORS];              // leituras atuais dos sensores
-int calibratedMIN[NUM_SENSORS];       // valores de calibracao
-int calibratedMAX[NUM_SENSORS];       // ^
+int calibratedMIN[NUM_SENSORS];       // valores de calibracao minimos
+int calibratedMAX[NUM_SENSORS];       // valores de calibracao maximos
 
 int saiu = 0;                         // indica se o robo nao esta na linha
 int leu_chegada = 0;                  // qtd de vezes SEGUIDAS que o sensor de chegada leu preto
@@ -30,6 +31,31 @@ int tensaoEsq = 0, tensaoDir = 0;
 int reduz = 0;                        // contador para deixar a velocidade reduzida
 int lastValue;                        // ultima posicao da linha lida
 unsigned int last_proportional = 0;
+
+//Variáveis de Contagem de pulsos
+const byte encoderPinD = 2;
+const byte encoderPinE = 3;
+volatile long int nPulsosD = 0;
+volatile long int nPulsosE = 0;
+
+//Variáveis de temporização:
+long int N_OVF = 65535L;                  //número máximo de pulsos contados pelo timer (16 bits)
+long int tAlvo = 1000L;                      //tempo a se cronometrar, em segundos
+long int FREQ_CLK = 16000000L;          //frequencia do clock
+long int PRESC = 1024L;                   //Valor do prescaler
+long int nAlvo = N_OVF -  (FREQ_CLK / PRESC) * ((float)tAlvo / 1000); //número de pulsos a se cronometrar
+
+//Variáveis para cálculo de velocidade
+volatile int velDreal = 0;
+volatile int velEreal = 0;
+const int nDentes = 10;
+const float Diametro = 2.33;//em cm
+const float kRoda = (1000 * PI*Diametro) / (nDentes*tAlvo); //tAlvo em ms
+const float kEnc = 1; //constante proporcional para o erro do encoder
+
+//caso use PID
+//int lastProporcionalE = 0, lastProporcionalD = 0;
+//int integralE = 0, integralE = 0;
 
 //********************************************************************
 
@@ -46,18 +72,14 @@ void loop() {
 
   int linePosition = readLine();
 
-  confereChegada();
+  //confereChegada();
 
   int erro = PID(linePosition);
 
-  if (erro < 0) {
-    tensaoEsq = VELMAX + erro;
-    tensaoDir = VELMAX;
-  }
-  else {
-    tensaoEsq = VELMAX;
-    tensaoDir = VELMAX - erro;
-  }
+  if(erro < 0)
+    ajustaVelocidade(VELMAX + erro, VELMAX);
+  if(erro > 0)
+    ajustaVelocidade(VELMAX + erro, VELMAX);
   
 //  if (confereCurva())
 //    reduz = 50;             //tempo para velocidade ficar reduzida
