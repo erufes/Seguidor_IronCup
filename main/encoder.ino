67e11601d06f5a25d3d91b24adea170ba6af812a
lksmaster <47121158+lksmaster@users.noreply.github.com>
2019-09-04 17:29:11 -0300

    Add files via upload

--- main/encoder.ino
+++ main/encoder.ino
@@ -1,4 +1,4 @@
-void initEncoder(){
+void initEncoder() {
   attachInterrupt(digitalPinToInterrupt(encoderPinD), countEncoderD, RISING);
   attachInterrupt(digitalPinToInterrupt(encoderPinE), countEncoderE, RISING);
 
@@ -10,8 +10,8 @@ void initEncoder(){
   TCCR1B &= B11111000; //zera os tres ultimos bits
   TCCR1B |= B00000101; //prescaler = 1024
   TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
-  
-  
+
+
   //Serial.print("kRoda = ");
   //Serial.println(kRoda);
 }
@@ -34,23 +34,45 @@ ISR(TIMER1_OVF_vect) {
   TCNT1 = nAlvo;    // preload timer
 }
 
+void verificaEncoderParado() {
+  if (velEreal != 0 || velDreal != 0) {
+    for (int i = 0; i < 9999; i++) {
+      digitalWrite(pin_led, HIGH);
+      delay(10);
+      digitalWrite(pin_led, LOW);
+      delay(10);
+    }
+  }
+}
+
+void verficaEncoderAndando(){
+  if (velEreal == 0 || velDreal == 0) {
+    for (int i = 0; i < 9999; i++) {
+      digitalWrite(pin_led, HIGH);
+      delay(10);
+      digitalWrite(pin_led, LOW);
+      delay(10);
+    }
+  }
+}
+
 //funcao que calcula a tensao que sera mandada para os motores
 //uitilizando as velocidades desejadas e as velocidades lidas pelo encoder
-void ajustaVelocidade(int velE, int velD){
+void ajustaVelocidade(int velE, int velD) {
   // o erro do encoder sera a diferenca das velocidades considerando
   // a posicao da linha, entao por exemplo, se a linha estiver no meio,
   // o erro sera a diferenca das velocidades, buscando deixar igual.
   int erroVelE = (velE - velEreal) * kEnc;
   int erroVelD = (velD - velDreal) * kEnc;
-  
-    tensaoDir = velE + erroVelE;
-    tensaoEsq = velD + erroVelD;
+
+  tensaoDir = velE + erroVelE;
+  tensaoEsq = velD + erroVelD;
 }
 
 /*
- * 
- * //PID do encoder
-void ajustaVelocidade(int velE, int velD){
+
+   //PID do encoder
+  void ajustaVelocidade(int velE, int velD){
 
   int proporcionalE = velE - velEreal;
   int derivativeE = proporcionalE - lastProporcionalE;
@@ -62,8 +84,8 @@ void ajustaVelocidade(int velE, int velD){
 
   int erroVelE = proporcionalE * kpEnc + derivativeE * kdEnc;
   int erroVelD = proporcionalD * kpEnc + derivativeD * kdEnc;
-  
+
   tensaoDir = velE + erroVelE;
   tensaoEsq = velD + erroVelD;
-}
+  }
 */
