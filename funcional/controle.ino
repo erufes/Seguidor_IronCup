34b8def3bc1c3b75c5445354e7577d6184dd9e58
hdogithub <43549329+hdogithub@users.noreply.github.com>
2019-09-14 11:54:46 -0300

    mudanca em constantes e velocidades
    valores para o primeiro teste

--- funcional/controle.ino
+++ funcional/controle.ino
@@ -36,7 +36,7 @@ int PID(unsigned int linePosition) {
   int proportional = (int)linePosition - ((NUM_SENSORS-1)*1000)/2; //erro proporcional = 'posição atual da linha' - 'posição central'
   int derivative = proportional - last_proportional;
   last_proportional = proportional;
-  int erro = proportional * 2 / 15 + derivative * 5 / 20; //22 16
+  int erro = proportional * 2 / 20 + derivative * 5 / 30; //15 20
   if (erro > VELMAX)
     erro = VELMAX;
   if (erro < -VELMAX)
@@ -64,7 +64,7 @@ void anda(int velE, int velD)
   } else if (velE >= 0 && velE < VELMIN) {
     digitalWrite(motorEsq[0], LOW);
     digitalWrite(motorEsq[1], HIGH);
-    analogWrite(motorEsq[2], (250 - 5 * velE));
+    analogWrite(motorEsq[2], (250 - 7 * velE));
     //Serial.print(-(250 - 5 * velE));
   } else {
     digitalWrite(motorEsq[0], LOW);
@@ -81,7 +81,7 @@ void anda(int velE, int velD)
   } else if (velD >= 0 && velD < VELMIN) {
     digitalWrite(motorDir[0], LOW);
     digitalWrite(motorDir[1], HIGH);
-    analogWrite(motorDir[2],(250 - 5 * velD));
+    analogWrite(motorDir[2],(250 - 7 * velD));
     //Serial.println(-(250 - 5 * velD));
   } else {
     digitalWrite(motorDir[0], LOW);
